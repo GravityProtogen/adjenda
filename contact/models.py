4e6de7ce57ddae07c6d3db8f0386e0396c3e055f
Gravity Proto <pedroarturcosta@hotmail.com>
2025-01-16 11:59:08 -0300

    Adição de usuarios até categorias

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# Depois
+# category (foreign key), show (boolean)
+# picture (imagem)
+
+# Depois
+# owner (foreign key)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
